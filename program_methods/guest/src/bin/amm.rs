f9cc9218312e60babfdc2439fb9a6d80b44b0c10
Andrea Franz <andrea@gravityblast.com>
2026-02-25 13:11:54 +0100

    feat(programs/amm): add swap exact output functionality

--- program_methods/guest/src/bin/amm.rs
+++ program_methods/guest/src/bin/amm.rs
@@ -131,6 +131,25 @@ fn main() {
                 token_definition_id_in,
             )
         }
+        Instruction::SwapExactOutput {
+            exact_amount_out,
+            max_amount_in,
+            token_definition_id_in,
+        } => {
+            let [pool, vault_a, vault_b, user_holding_a, user_holding_b] = pre_states
+                .try_into()
+                .expect("SwapExactOutput instruction requires exactly five accounts");
+            amm_program::swap::swap_exact_output(
+                pool,
+                vault_a,
+                vault_b,
+                user_holding_a,
+                user_holding_b,
+                exact_amount_out,
+                max_amount_in,
+                token_definition_id_in,
+            )
+        }
     };
 
     ProgramOutput::new(instruction_words, pre_states_clone, post_states)

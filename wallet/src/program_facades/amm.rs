3esmit/lssa

feat(programs/amm): add swap exact output functionality

wallet/src/program_facades/amm.rs
@@ -209,6 +209,111 @@ impl Amm<'_> {
             .await?)
     }
 
+    pub async fn send_swap_exact_output(
+        &self,
+        user_holding_a: AccountId,
+        user_holding_b: AccountId,
+        exact_amount_out: u128,
+        max_amount_in: u128,
+        token_definition_id_in: AccountId,
+    ) -> Result<SendTxResponse, ExecutionFailureKind> {
+        let instruction = amm_core::Instruction::SwapExactOutput {
+            exact_amount_out,
+            max_amount_in,
+            token_definition_id_in,
+        };
+        let program = Program::amm();
+        let amm_program_id = Program::amm().id();
+
+        let user_a_acc = self
+            .0
+            .get_account_public(user_holding_a)
+            .await
+            .map_err(|_| ExecutionFailureKind::SequencerError)?;
+        let user_b_acc = self
+            .0
+            .get_account_public(user_holding_b)
+            .await
+            .map_err(|_| ExecutionFailureKind::SequencerError)?;
+
+        let definition_token_a_id = TokenHolding::try_from(&user_a_acc.data)
+            .map_err(|_| ExecutionFailureKind::AccountDataError(user_holding_a))?
+            .definition_id();
+        let definition_token_b_id = TokenHolding::try_from(&user_b_acc.data)
+            .map_err(|_| ExecutionFailureKind::AccountDataError(user_holding_b))?
+            .definition_id();
+
+        let amm_pool =
+            compute_pool_pda(amm_program_id, definition_token_a_id, definition_token_b_id);
+        let vault_holding_a = compute_vault_pda(amm_program_id, amm_pool, definition_token_a_id);
+        let vault_holding_b = compute_vault_pda(amm_program_id, amm_pool, definition_token_b_id);
+
+        let account_ids = vec![
+            amm_pool,
+            vault_holding_a,
+            vault_holding_b,
+            user_holding_a,
+            user_holding_b,
+        ];
+
+        let account_id_auth;
+
+        // Checking, which account are associated with TokenDefinition
+        let token_holder_acc_a = self
+            .0
+            .get_account_public(user_holding_a)
+            .await
+            .map_err(|_| ExecutionFailureKind::SequencerError)?;
+        let token_holder_acc_b = self
+            .0
+            .get_account_public(user_holding_b)
+            .await
+            .map_err(|_| ExecutionFailureKind::SequencerError)?;
+
+        let token_holder_a = TokenHolding::try_from(&token_holder_acc_a.data)
+            .map_err(|_| ExecutionFailureKind::AccountDataError(user_holding_a))?;
+        let token_holder_b = TokenHolding::try_from(&token_holder_acc_b.data)
+            .map_err(|_| ExecutionFailureKind::AccountDataError(user_holding_b))?;
+
+        if token_holder_a.definition_id() == token_definition_id_in {
+            account_id_auth = user_holding_a;
+        } else if token_holder_b.definition_id() == token_definition_id_in {
+            account_id_auth = user_holding_b;
+        } else {
+            return Err(ExecutionFailureKind::AccountDataError(
+                token_definition_id_in,
+            ));
+        }
+
+        let nonces = self
+            .0
+            .get_accounts_nonces(vec![account_id_auth])
+            .await
+            .map_err(|_| ExecutionFailureKind::SequencerError)?;
+
+        let signing_key = self
+            .0
+            .storage
+            .user_data
+            .get_pub_account_signing_key(account_id_auth)
+            .ok_or(ExecutionFailureKind::KeyNotFoundError)?;
+
+        let message = nssa::public_transaction::Message::try_new(
+            program.id(),
+            account_ids,
+            nonces,
+            instruction,
+        )
+        .unwrap();
+
+        let witness_set =
+            nssa::public_transaction::WitnessSet::for_message(&message, &[signing_key]);
+
+        let tx = nssa::PublicTransaction::new(message, witness_set);
+
+        Ok(self.0.sequencer_client.send_tx_public(tx).await?)
+    }
+
     pub async fn send_add_liquidity(
         &self,
         user_holding_a: AccountId,

soloking1412/logos-execution-zone

wallet/src/program_facades/amm.rs
@@ -209,6 +209,115 @@ impl Amm<'_> {
             .await?)
     }
 
+    pub async fn send_swap_exact_output(
+        &self,
+        user_holding_a: AccountId,
+        user_holding_b: AccountId,
+        exact_amount_out: u128,
+        max_amount_in: u128,
+        token_definition_id_in: AccountId,
+    ) -> Result<HashType, ExecutionFailureKind> {
+        let instruction = amm_core::Instruction::SwapExactOutput {
+            exact_amount_out,
+            max_amount_in,
+            token_definition_id_in,
+        };
+        let program = Program::amm();
+        let amm_program_id = Program::amm().id();
+
+        let user_a_acc = self
+            .0
+            .get_account_public(user_holding_a)
+            .await
+            .map_err(ExecutionFailureKind::SequencerError)?;
+        let user_b_acc = self
+            .0
+            .get_account_public(user_holding_b)
+            .await
+            .map_err(ExecutionFailureKind::SequencerError)?;
+
+        let definition_token_a_id = TokenHolding::try_from(&user_a_acc.data)
+            .map_err(|_err| ExecutionFailureKind::AccountDataError(user_holding_a))?
+            .definition_id();
+        let definition_token_b_id = TokenHolding::try_from(&user_b_acc.data)
+            .map_err(|_err| ExecutionFailureKind::AccountDataError(user_holding_b))?
+            .definition_id();
+
+        let amm_pool =
+            compute_pool_pda(amm_program_id, definition_token_a_id, definition_token_b_id);
+        let vault_holding_a = compute_vault_pda(amm_program_id, amm_pool, definition_token_a_id);
+        let vault_holding_b = compute_vault_pda(amm_program_id, amm_pool, definition_token_b_id);
+
+        let account_ids = vec![
+            amm_pool,
+            vault_holding_a,
+            vault_holding_b,
+            user_holding_a,
+            user_holding_b,
+        ];
+
+        let account_id_auth;
+
+        // Checking, which account are associated with TokenDefinition
+        let token_holder_acc_a = self
+            .0
+            .get_account_public(user_holding_a)
+            .await
+            .map_err(ExecutionFailureKind::SequencerError)?;
+        let token_holder_acc_b = self
+            .0
+            .get_account_public(user_holding_b)
+            .await
+            .map_err(ExecutionFailureKind::SequencerError)?;
+
+        let token_holder_a = TokenHolding::try_from(&token_holder_acc_a.data)
+            .map_err(|_err| ExecutionFailureKind::AccountDataError(user_holding_a))?;
+        let token_holder_b = TokenHolding::try_from(&token_holder_acc_b.data)
+            .map_err(|_err| ExecutionFailureKind::AccountDataError(user_holding_b))?;
+
+        if token_holder_a.definition_id() == token_definition_id_in {
+            account_id_auth = user_holding_a;
+        } else if token_holder_b.definition_id() == token_definition_id_in {
+            account_id_auth = user_holding_b;
+        } else {
+            return Err(ExecutionFailureKind::AccountDataError(
+                token_definition_id_in,
+            ));
+        }
+
+        let nonces = self
+            .0
+            .get_accounts_nonces(vec![account_id_auth])
+            .await
+            .map_err(ExecutionFailureKind::SequencerError)?;
+
+        let signing_key = self
+            .0
+            .storage
+            .user_data
+            .get_pub_account_signing_key(account_id_auth)
+            .ok_or(ExecutionFailureKind::KeyNotFoundError)?;
+
+        let message = nssa::public_transaction::Message::try_new(
+            program.id(),
+            account_ids,
+            nonces,
+            instruction,
+        )
+        .unwrap();
+
+        let witness_set =
+            nssa::public_transaction::WitnessSet::for_message(&message, &[signing_key]);
+
+        let tx = nssa::PublicTransaction::new(message, witness_set);
+
+        Ok(self
+            .0
+            .sequencer_client
+            .send_transaction(NSSATransaction::Public(tx))
+            .await?)
+    }
+
     pub async fn send_add_liquidity(
         &self,
         user_holding_a: AccountId,